6cf90e2f0b92af72b8ece826b4ce2decf46f38a7
martin-netender <79976002+martin-netender@users.noreply.github.com>
2021-03-03 14:19:56 +0100

    :white_check_mark: update test
    Fix test

--- src/components/elements/Copyright/Copyright.test.tsx
+++ src/components/elements/Copyright/Copyright.test.tsx
@@ -5,7 +5,7 @@ import Copyright from './Copyright';
 describe('<Copyright> ', () => {
     const { getByText} = render(<Copyright />);
     test('renders correct text', () => {
-        const text = getByText(/No Copyright ©/i);
+        const text = getByText(/Copyright ©/i);
         expect(text).toBeInTheDocument();
     });
 
