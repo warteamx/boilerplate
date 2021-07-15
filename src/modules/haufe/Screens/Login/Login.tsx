0254a6808fa9ac41fdad7ae9e8e9cacb23819937
martin-netender <martinrg33@gmail.com>
2021-07-15 13:28:35 +0200

    :construction: work in progress

--- src/modules/haufe/Screens/Login/Login.tsx
+++ src/modules/haufe/Screens/Login/Login.tsx
@@ -1,24 +1,73 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement , useState, useEffect} from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase'
+import { userInfo } from 'os';
 
-const firebase = require('firebase');
-const firebaseui = require('firebaseui');
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyBxwQlDE8omqzejYDuo_ZmycEh8QQjM5GY",
+  authDomain: "boilerplate-mern.firebaseapp.com",
+  projectId: "boilerplate-mern",
+  storageBucket: "boilerplate-mern.appspot.com",
+  messagingSenderId: "111523193499",
+  appId: "1:111523193499:web:684e369f7592b15e465c3e",
+  measurementId: "G-BXFLXX6SB0"
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  // signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  }
+};
 
 function Login(): ReactElement {
 
-  let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    // Other config options...
-  });
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  // Listen to the Firebase Auth state and set the local state.
+  let user = {displayName: "ñoco"}
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+      user = user
+      console.log(user)
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <div style={{border: "1px solid green"}}>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+
+        {user?.displayName}
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <div id="firebaseui-auth-container"></div>
-      <div id="loader">Loading...</div>
+    <div style={{border: "1px solid green"}}>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+
+      {user?.displayName}
     </div>
   )
 }
