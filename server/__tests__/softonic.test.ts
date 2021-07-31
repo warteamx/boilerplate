de5b629e526f5b89f2c62d207e3a9a5849239a10
martinrebo <martinrg33@gmail.com>
2021-07-31 12:14:11 +0200

    :white_check_mark: Add tests to api endpoint

--- server/__tests__/softonic.test.ts
+++ server/__tests__/softonic.test.ts
@@ -1,17 +1,27 @@
-const request = require('supertest')
-const app = require('../server')
+export {};
 
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })
+const request = require("supertest")("http://localhost:5000/api/softonic");
+const expect = require("chai").expect;
 
-  it('should respond with json', (done)=> {
-    request(app)
-    .get('/programs')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
+describe("GET /programs", function () {
+  it("returns all programs, limited to 3 per page", async function () {
+    const response = await request.get("/programs");
 
-  })
-})
+    expect(response.status).to.eql(200);
+    expect(response.body.data.length).to.eql(3);
+  });
+
+  it("returns one program, ", async function () {
+    const response = await request.get("/programs/21824");
+
+    expect(response.status).to.eql(200);
+    expect(response.body.data.id).to.eql("21824");
+  });
+
+  it("returns error 404 of program not found, ", async function () {
+    const response = await request.get("/programs/21824d");
+
+    expect(response.status).to.eql(404);
+    expect(response.body.data.length).to.eql(0);
+  });
+});
